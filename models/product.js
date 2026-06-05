476240dacaef7e7c7b1bd6f11486bbcceeeaabf4
André Espinoza <ae95caba@gmail.com>
2026-06-05 20:38:40 -0300

    add subType field to Product schema

--- models/product.js
+++ models/product.js
@@ -15,6 +15,18 @@ const ProductSchema = new Schema({
     enum: ["frozen-treat", "ice-cream", "add-on"],
     required: true,
   },
+  subType: {
+    type: String,
+    enum: [
+    "lata",
+    "petaca",
+    "vino",
+    "fernet",
+    "licor",
+    "sin-alcohol"
+  ],
+    required: false,
+  },
 });
 
 // Export model
